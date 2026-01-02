87e69dbbf7bee5926c3113c032e3b5b628f57756
fancoilaya <fancoilaya@gmail.com>
2026-01-02 17:05:14 +0100

    Update mobFactory.js

--- game/mobs/mobFactory.js
+++ game/mobs/mobFactory.js
@@ -1,5 +1,4 @@
 // game/mobs/mobFactory.js
-
 import { MOB_TYPES } from './mobRegistry'
 
 export function spawnMob(scene, typeKey, x, y) {
@@ -11,6 +10,7 @@ export function spawnMob(scene, typeKey, x, y) {
     def.size,
     def.color
   )
+  mob.setDepth(10) // mobs
 
   scene.physics.add.existing(mob)
   mob.body.setCollideWorldBounds(true)
@@ -21,14 +21,18 @@ export function spawnMob(scene, typeKey, x, y) {
   mob.maxHp = def.hp
   mob.speed = def.speed
   mob.damage = def.damage
+  mob.size = def.size
 
+  // HP BAR (ALWAYS ABOVE MOB)
   mob.hpBar = scene.add.rectangle(
     mob.x,
-    mob.y - (def.size + 6),
-    def.size * 1.5,
+    mob.y - (def.size + 10),
+    def.size * 1.6,
     4,
     0xff0000
-  ).setOrigin(0.5)
+  )
+  mob.hpBar.setOrigin(0.5)
+  mob.hpBar.setDepth(20) // above mob
 
   scene.enemies.add(mob)
   return mob
