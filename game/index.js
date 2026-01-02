ee64c30da9c960dceccd51e561af61e1783c4c39
fancoilaya <fancoilaya@gmail.com>
2026-01-02 01:01:22 +0100

    Create index.js

---
+++ game/index.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser'
+import ArenaScene from './scenes/ArenaScene'
+
+export function startArenaGame(containerId, onGameOver) {
+  return new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: containerId,
+    width: 800,
+    height: 500,
+    backgroundColor: '#0b0b0b',
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false
+      }
+    },
+    scene: [
+      new ArenaScene(onGameOver)
+    ],
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+  })
+}
