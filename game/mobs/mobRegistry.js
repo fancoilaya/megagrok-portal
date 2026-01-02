38d1034a1536996c2bf1d298e554bf5f76df79c5
fancoilaya <fancoilaya@gmail.com>
2026-01-02 17:37:59 +0100

    Update mobRegistry.js

--- game/mobs/mobRegistry.js
+++ game/mobs/mobRegistry.js
@@ -3,36 +3,31 @@
 export const MOB_TYPES = {
   grunt: {
     id: 'grunt',
-    hp: 30,
+    hp: 35,
     speed: 80,
-    damage: 8,
+    damage: 6,
     size: 10,
     color: 0xff4444,
-    attackRange: 18,
-    followRange: 9999
+    attackRange: 18
   },
 
   runner: {
     id: 'runner',
     hp: 22,
     speed: 120,
-    damage: 6,
+    damage: 5,
     size: 9,
     color: 0xff7777,
-    attackRange: 15,
-    followRange: 9999
+    attackRange: 15
   },
 
   tank: {
     id: 'tank',
-    hp: 120,
-    speed: 40,
-    damage: 15,
-    size: 16,
+    hp: 260,              // ⬆ boss HP
+    speed: 38,
+    damage: 18,           // ⬆ boss damage
+    size: 18,
     color: 0xaa2222,
-    attackRange: 20,
-    followRange: 9999
+    attackRange: 26       // ⬆ boss reach
   }
-
-  // ranged mobs will be added later cleanly
 }
